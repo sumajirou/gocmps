a7c3b670020c7de45f6094d1b6d05c9a3242fa92
Sumajirou <4375490+sumajirou@users.noreply.github.com>
2020-04-23 02:55:08 +0900

    演算子 '+' と '-' を追加

--- gocmps.go
+++ gocmps.go
@@ -3,23 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"regexp"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		// panicの使い方これで正しいのかな……？
 		panic("引数の個数が正しくありません")
 	}
 
-	value, err := strconv.Atoi(os.Args[1])
+	s := os.Args[1]
+	// 数値 | + | - | それ以外
+	re, err := regexp.Compile("\\d+|[+-]|[^\\d+-]+")
 	if err != nil {
-		panic("引数が数値ではありません。")
+		panic("正規表現が不正です。")
+	}
+
+	token := []string{}
+	for _, v := range re.FindAllStringSubmatch(s, -1) {
+		token = append(token, v[0])
 	}
 
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global main\n")
 	fmt.Printf("main:\n")
-	fmt.Printf("  mov rax, %d\n", value)
+	fmt.Printf("  mov rax, %s\n", token[0])
+	token = token[1:]
+	for len(token) > 0 {
+		if token[0] == "+" {
+			fmt.Printf("  add rax, %s\n", token[1])
+		} else if token[0] == "-" {
+			fmt.Printf("  sub rax, %s\n", token[1])
+		} else {
+			panic(fmt.Sprintf("不正なトークンです。: %s", token[1]))
+		}
+		token = token[2:]
+	}
 	fmt.Printf("  ret\n")
 }
